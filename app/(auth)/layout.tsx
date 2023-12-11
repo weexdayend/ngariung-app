34144e995de82bdda2ed6816a7f8f4a58fca6529
Irvan Gerhana <weexdayend@gmail.com>
2023-12-11 23:27:32 +0700

    Ngariung App

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+import { dark } from "@clerk/themes"
+
+import '../globals.css'
+
+export const metadata = {
+    title: 'Ngariung',
+    description: 'A Next.js 13 Meta Ngariung Application'
+}
+
+const inter = Inter({ subsets: ["latin"] })
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <ClerkProvider
+            appearance={{
+                baseTheme: dark,
+            }}
+        >
+            <html lang='en'>
+                <body className={`${inter.className} bg-dark-1`}>{children}</body>
+            </html>
+        </ClerkProvider>
+    )
+}
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,53 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+import '../globals.css'
+
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from "@clerk/themes";
+
+import TopBar from '@/components/shared/TopBar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import BottomBar from '@/components/shared/BottomBar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Ngariung',
+  description: 'A Next.js 13 Meta Ngariung Application'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en">
+        <body className={inter.className}>
+          <TopBar />
+          
+            <main className="flex flex-row">
+              <LeftSidebar />
+                
+                <section className="main-container">
+                  <div className="w-full max-w-4xl">
+                    {children}
+                  </div>
+                </section>
+
+              <RightSidebar />
+            </main>
+
+          <BottomBar />
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
