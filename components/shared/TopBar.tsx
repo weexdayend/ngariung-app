f415994922a0228f4609b872717702de5e58dbd3
Irvan Gerhana <weexdayend@gmail.com>
2023-12-11 23:44:19 +0700

    Fixing Top Bar

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 function Topbar() {
   return (
     <nav className='topbar'>
-      <Link href='/' className='flex items-center gap-4'>
-        <Image src='/logo.svg' alt='logo' width={28} height={28} />
-        <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
+      <Link href='/' className='flex items-center gap-2'>
+        <Image src='/assets/logo.svg' alt='logo' width={28} height={28} />
+        <p className='text-heading3-bold text-light-1 max-xs:hidden'>Ngariung</p>
       </Link>
 
       <div className='flex items-center gap-1'>
