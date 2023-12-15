ecca827ea82ed0c51acbee6eccba6a5c7c92023d
Irvan Gerhana <weexdayend@gmail.com>
2023-12-15 08:29:33 +0700

    fixing workshop

--- app/(root)/workshop/[id]/page.tsx
+++ app/(root)/workshop/[id]/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import Image from "next/image";
 
-import { HiOutlineCalendar } from 'react-icons/hi2';
+import { HiOutlineCalendar, HiOutlineMap, HiOutlineUsers } from 'react-icons/hi2';
 import { usePathname } from 'next/navigation';
 import { useAuth } from '@clerk/nextjs';
 
@@ -40,18 +40,18 @@ const Page = () => {
       <div className="relative -top-8 w-full px-14 pb-6">
         {
           isActive ? (
-            <div className="flex flex-row gap-4 items-center justify-between px-1.5 py-0.5 rounded-2xl bg-yellow-400">
-              <div className="px-4 py-4">
+            <div className="flex flex-row gap-4 items-center justify-between px-2 py-2 rounded-2xl bg-yellow-400">
+              <div className="px-4">
                 <p className="text-xs text-gray-800">Kamu telah menyelesaikan seminar dan kuis yang ada, silahkan untuk download sertifikat yang sudah kami sediakan!</p>
               </div>
-              <button className="px-4 py-4 text-sm bg-gray-800 text-white rounded-xl">Download</button>
+              <button className="px-4 py-2 text-sm bg-gray-800 text-white rounded-xl">Download</button>
             </div>
           ) : (
-            <div className="flex flex-row gap-4 items-center justify-between px-1.5 py-0.5 rounded-2xl bg-blue-100">
-              <div className="px-4 py-4">
+            <div className="flex flex-row gap-4 items-center justify-between px-2 py-2 rounded-2xl bg-blue-100">
+              <div className="px-4">
                 <p className="text-xs text-gray-800">Booking seat kamu untuk mengikuti seminar ini, dan ikuti seluruh rangkaian kegiatan seminar dan download sertifikat-nya!</p>
               </div>
-              <button className="px-4 py-4 text-sm bg-gray-800 text-white rounded-xl">Booking</button>
+              <button className="px-4 py-2 text-sm bg-gray-800 text-white rounded-xl">Booking</button>
             </div>
           )
         }
@@ -60,6 +60,59 @@ const Page = () => {
           <br /><br />
           One of the fundamental concepts in AI is machine learning, a subset of AI that involves building systems capable of learning from data. Machine learning algorithms enable computers to recognize patterns, make predictions, and improve their performance over time without explicit programming. This ability to learn and adapt is what sets AI apart and makes it so powerful.
         </p>
+
+        <div className="flex flex-col w-full gap-6 px-4 py-6 rounded-xl border border-gray-200 mt-10">
+          <h1 className="text-lg text-gray-700">Informasi</h1>
+          <div className="flex flex-row items-center justify-start gap-2">
+            <HiOutlineMap className="h-5 w-5 text-gray-500" />
+            <p className="text-sm font-light text-gray-500">Lokasi Seminar di Gedung A.</p>
+          </div>
+          <div className="flex flex-row items-center justify-start gap-2">
+            <HiOutlineUsers className="h-5 w-5 text-gray-500" />
+            <p className="text-sm font-light text-gray-500">45/50 Seat</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-full gap-6 px-4 py-6 rounded-xl border border-gray-200 mt-10">
+          <h1 className="text-lg text-gray-700">Pemaparan</h1>
+          <div className="flex flex-col">
+            <div className="flex flex-row items-start justify-start gap-4 border-b border-gray-200 py-4">
+              <p className="text-sm text-indigo-600">09:00 - 10:00</p>
+              <div className="flex flex-col">
+                <p className="text-sm font-semibold text-gray-700">Pengenalan AI</p>
+                <p className="text-xs font-light text-gray-700">Dwi Sasono</p>
+              </div>
+            </div>
+            <div className="flex flex-row items-start justify-start gap-4 border-b border-gray-200 py-4">
+              <p className="text-sm text-indigo-600">09:00 - 10:00</p>
+              <div className="flex flex-col">
+                <p className="text-sm font-semibold text-gray-700">Pengenalan AI</p>
+                <p className="text-xs font-light text-gray-700">Dwi Sasono</p>
+              </div>
+            </div>
+            <div className="flex flex-row items-start justify-start gap-4 border-b border-gray-200 py-4">
+              <p className="text-sm text-indigo-600">09:00 - 10:00</p>
+              <div className="flex flex-col">
+                <p className="text-sm font-semibold text-gray-700">Pengenalan AI</p>
+                <p className="text-xs font-light text-gray-700">Dwi Sasono</p>
+              </div>
+            </div>
+            <div className="flex flex-row items-start justify-start gap-4 border-b border-gray-200 py-4">
+              <p className="text-sm text-indigo-600">09:00 - 10:00</p>
+              <div className="flex flex-col">
+                <p className="text-sm font-semibold text-gray-700">Pengenalan AI</p>
+                <p className="text-xs font-light text-gray-700">Dwi Sasono</p>
+              </div>
+            </div>
+            <div className="flex flex-row items-start justify-start gap-4 py-4">
+              <p className="text-sm text-indigo-600">09:00 - 10:00</p>
+              <div className="flex flex-col">
+                <p className="text-sm font-semibold text-gray-700">Pengenalan AI</p>
+                <p className="text-xs font-light text-gray-700">Dwi Sasono</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
